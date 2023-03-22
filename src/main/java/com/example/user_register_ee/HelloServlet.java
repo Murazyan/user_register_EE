d200aa2f709954f30d1d0bb1c65dd3992a9a0e72
samvle-tamazyan <sam.tamazyan2001@gmail.com>
2023-03-22 21:08:40 +0400

    Initial commit

---
+++ src/main/java/com/example/user_register_ee/HelloServlet.java
@@ -0,0 +1,94 @@
+package com.example.user_register_ee;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+
+
+@WebServlet(name = "helloServlet", value = "/poxos", loadOnStartup = 1)
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String test1 = request.getParameter("test1");
+        String  browserDetails  =   request.getHeader("User-Agent");
+        String  userAgent       =   browserDetails;
+        String  user            =   userAgent.toLowerCase();
+
+        String os = "";
+        String browser = "";
+
+        //=================OS=======================
+        if (userAgent.toLowerCase().indexOf("windows") >= 0 )
+        {
+            os = "Windows";
+        } else if(userAgent.toLowerCase().indexOf("mac") >= 0)
+        {
+            os = "Mac";
+        } else if(userAgent.toLowerCase().indexOf("x11") >= 0)
+        {
+            os = "Unix";
+        } else if(userAgent.toLowerCase().indexOf("android") >= 0)
+        {
+            os = "Android";
+        } else if(userAgent.toLowerCase().indexOf("iphone") >= 0)
+        {
+            os = "IPhone";
+        }else{
+            os = "UnKnown, More-Info: "+userAgent;
+        }
+        //===============Browser===========================
+        if (user.contains("msie"))
+        {
+            String substring=userAgent.substring(userAgent.indexOf("MSIE")).split(";")[0];
+            browser=substring.split(" ")[0].replace("MSIE", "IE")+"-"+substring.split(" ")[1];
+        } else if (user.contains("safari") && user.contains("version"))
+        {
+            browser=(userAgent.substring(userAgent.indexOf("Safari")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+        } else if ( user.contains("opr") || user.contains("opera"))
+        {
+            if(user.contains("opera"))
+                browser=(userAgent.substring(userAgent.indexOf("Opera")).split(" ")[0]).split("/")[0]+"-"+(userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+            else if(user.contains("opr"))
+                browser=((userAgent.substring(userAgent.indexOf("OPR")).split(" ")[0]).replace("/", "-")).replace("OPR", "Opera");
+        } else if (user.contains("chrome"))
+        {
+            browser=(userAgent.substring(userAgent.indexOf("Chrome")).split(" ")[0]).replace("/", "-");
+        } else if ((user.indexOf("mozilla/7.0") > -1) || (user.indexOf("netscape6") != -1)  || (user.indexOf("mozilla/4.7") != -1) || (user.indexOf("mozilla/4.78") != -1) || (user.indexOf("mozilla/4.08") != -1) || (user.indexOf("mozilla/3") != -1) )
+        {
+            //browser=(userAgent.substring(userAgent.indexOf("MSIE")).split(" ")[0]).replace("/", "-");
+            browser = "Netscape-?";
+
+        } else if (user.contains("firefox"))
+        {
+            browser=(userAgent.substring(userAgent.indexOf("Firefox")).split(" ")[0]).replace("/", "-");
+        } else if(user.contains("rv"))
+        {
+            browser="IE-" + user.substring(user.indexOf("rv") + 3, user.indexOf(")"));
+        } else
+        {
+            browser = "UnKnown, More-Info: "+userAgent;
+        }
+        response.setContentType("text/html");
+//
+//        // Hello
+        PrintWriter out = response.getWriter();
+        out.write(test1);
+        out.write("<br>");
+        out.write(String.format("Your system = %s, client type: %s", os,browser));
+//        out.println("<html><body>");
+//        out.println("<h1>" + message + "</h1>");
+//        out.println("</body></html>");
+    }
+
+    public void destroy() {
+        System.out.println("server stoped: destoy method for servel");
+    }
+}
